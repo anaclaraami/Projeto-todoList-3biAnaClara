29af8952184c5810404eb482ef7f4330feef8afd
Ana Clara Amianti <canaldalara89@gmail.com>
2025-08-21 12:00:05 -0300

    [feat] Input Tarefa

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text,TextInput, View } from "react-native";
 import logo from "./assets/icon.png";
+import { useState } from "react";
+import btnAdd from "./assets/pngegg(14).png"
 
 export default function App() {
+  const[tarefa, setTarefa] = useState("")
   return (
     <View style={styles.container}>
       <View>
         <Image source={logo} style={styles.logo}/>
+        <Text> TODO list</Text>
+      </View>
+      <View>
+        <TextInput placeholder="Entre com a tarefa"
+         value={tarefa}
+         onChangeText={setTarefa}
+        />
+        <Image source={btnAdd}/>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -23,5 +34,13 @@ const styles = StyleSheet.create({
   logo: {
     height: 128,
     width : 128,
+  },
+  logo: {
+    height:128,
+    width:128,
+  },
+  btnAdd: {
+    width:32,
+    height:32,
   }
 });
